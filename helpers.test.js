773e27ce66213836ec9e1e1992fcb1761d9de91e
ozgenur00 <ozgenurcatal00@gmail.com>
2024-03-11 17:02:30 -0400

    express calculator exercise added

---
+++ helpers.test.js
@@ -0,0 +1,30 @@
+const {
+    findMean,
+    findMedian,
+    findMode
+} = require('./helpers');
+
+
+describe("#findMean", function () {
+    it("finds the mean of an empty array", function () { 
+      expect(findMean([])).toEqual(0)
+    })
+    it("finds the mean of an array of numbers", function () { 
+      expect(findMean([1,-1,4,2])).toEqual(1.5)
+    })
+  })
+
+describe('#findMedian', function() {
+    it("finds the median of an even set", () => {
+        expect(findMedian([1, -1, 4, 2])).toEqual(1.5)
+    })
+    it("finds the median of an odd set", () => {
+        expect(findMedian([1, -1, 4])).toEqual(1)
+    })
+})
+
+describe("#findMode", function () {
+    it("finds the mode", function () { 
+      expect(findMode([1,1,1,2,2,3])).toEqual(1)
+    })
+  })
